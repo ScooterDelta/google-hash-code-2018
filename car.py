ef2c607f2d065906d80ded6aff83531a65f5d4d4
jcqvisser <jcqvisser@gmail.com>
2018-03-01 20:40:12 +0200

    Update car to do suff

--- car.py
+++ car.py
@@ -1,16 +1,26 @@
 class Car:
-  def __init__(self, x, y):
+  def __init__(self, start_pos):
     self.trips = []
-    self.x
-    self.y
+    self.start_pos = start_pos
     self.time
     self.score_calc
 
   def add_trip(self, trip):
     self.trips += [trip]
 
-  def can_add_trip(trip):
 
-  def distance_to_trip(self.trip):
-    return abs(self.x - trip.start_x ) + abs(self.y - tip.start_y)
+  def can_add_trip(trip):
+    ( self.trip[-1].end_pos.distance_to(trip.start_pos) + all_trips_steps ) <= trip.latest_start
+
+  def all_trips_steps(self):
+    total_steps = 0
+    current_pos = self.start_pos
+    for scheduled_trip in self.trips:
+      total_steps += trip(curent_pos, scheduled_trip.start_pos).duration + scheduled_trip.duration
+      current_pos = scheduled_trip.end_pos
+
+    return total_steps
+
+
+
 
